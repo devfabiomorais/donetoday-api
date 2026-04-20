278f016ddb7c692414716e8b83fabd1ac1fca4fb
devfabiomorais <devfabiomorais@gmail.com>
2026-04-19 21:12:46 -0300

    feat: add error handling middleware and try/catch on controllers

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import userRoutes from './modules/users/users.routes'
+import { errorHandler } from './middlewares/errorHandler'
 
 const app = express()
 
 app.use(express.json())
 app.use('/users', userRoutes)
+app.use(errorHandler)
 
 export default app
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,36 +1,56 @@
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 import { findAllUsers, findUserById, createUser, updateUser, deleteUser } from './users.service'
 
-export const getUsers = async (req: Request, res: Response) => {
-  const users = await findAllUsers()
-  res.json(users)
-}
-
-export const getUserById = async (req: Request, res: Response) => {
-  const { id } = req.params as { id: string }
-  const user = await findUserById(id)
-  if (!user) {
-    res.status(404).json({ message: 'User not found' })
-    return
+export const getUsers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await findAllUsers()
+    res.json(users)
+  } catch (error) {
+    next(error)
   }
-  res.json(user)
 }
 
-export const postUser = async (req: Request, res: Response) => {
-  const { name, email } = req.body
-  const user = await createUser(name, email)
-  res.status(201).json(user)
+export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params as { id: string }
+    const user = await findUserById(id)
+    if (!user) {
+      res.status(404).json({ message: 'User not found' })
+      return
+    }
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
 }
 
-export const putUser = async (req: Request, res: Response) => {
-  const { id } = req.params as { id: string }
-  const { name, email } = req.body
-  const user = await updateUser(id, name, email)
-  res.json(user)
+export const postUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { name, email } = req.body
+    const user = await createUser(name, email)
+    res.status(201).json(user)
+  } catch (error) {
+    next(error)
+  }
 }
 
-export const removeUser = async (req: Request, res: Response) => {
-  const { id } = req.params as { id: string }
-  await deleteUser(id)
-  res.status(204).send()
+export const putUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params as { id: string }
+    const { name, email } = req.body
+    const user = await updateUser(id, name, email)
+    res.json(user)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const removeUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { id } = req.params as { id: string }
+    await deleteUser(id)
+    res.status(204).send()
+  } catch (error) {
+    next(error)
+  }
 }
