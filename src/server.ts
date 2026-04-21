be56347f68a96b363b92ffd667797b6e4b82d272
devfabiomorais <devfabiomorais@gmail.com>
2026-04-21 18:48:43 -0300

    chore: prepare project for Railway deployment (env config, prisma setup, dynamic port)

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import './lib/env'
 import app from './app'
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
